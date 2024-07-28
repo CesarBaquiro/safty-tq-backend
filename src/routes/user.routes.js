f5f6960c82678ab52391f67d1ff6fd2f2146a4c0
CesarBaquiro <cmbm.kf@gmail.com>
2024-07-28 17:40:33 -0500

    Corrections

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 // -------------------SOLICITUDES HTTP
 
 // Obtener todos los usuarios
-router.get("/usuarios", async (req, res) => {
+router.get("/staffcontrol/usuarios", async (req, res) => {
     try {
         const [rows, fields] = await db.query("SELECT * FROM user");
         res.json(rows);
@@ -78,21 +78,28 @@ router.get("/usuarios/numeroCompetidor/:num", async (req, res) => {
 
             // Consulta para obtener las alergias del usuario
             const [allergyRows, allergyFields] = await db.query(
-                "SELECT * FROM safty_tq_db.allergy WHERE user_id = ?",
+                "SELECT allergy_info FROM allergy WHERE user_id = ?",
                 [userId]
             );
 
             // Consulta para obtener los contactos del usuario
             const [contactRows, contactFields] = await db.query(
-                "SELECT * FROM safty_tq_db.contact WHERE user_id = ?",
+                "SELECT contact_reference, contact_info FROM contact WHERE user_id = ?",
                 [userId]
             );
 
-            // Añadir las alergias y contactos al objeto del usuario
+            // Consulta para obtener los registros de pruebas del usuario
+            const [testingRecordsRows, testingRecordsFields] = await db.query(
+                "SELECT test_image, type FROM testing_records WHERE user_id = ?",
+                [userId]
+            );
+
+            // Añadir las alergias, contactos y registros de pruebas al objeto del usuario
             user.allergies = allergyRows;
             user.contacts = contactRows;
+            user.testingRecords = testingRecordsRows;
 
-            // Devolver el usuario con las alergias y contactos
+            // Devolver el usuario con las alergias, contactos y registros de pruebas
             res.json(user);
         } else {
             res.status(404).json({
@@ -107,6 +114,56 @@ router.get("/usuarios/numeroCompetidor/:num", async (req, res) => {
         res.status(500).json({ error: "Error interno del servidor" });
     }
 });
+
+// Buscar todas las pruebas / test registradas
+router.get("/staffcontrol/usuarios/testing-records", async (req, res) => {
+    try {
+        const [rows, fields] = await db.query("SELECT * FROM testing_records");
+        res.json(rows);
+    } catch (err) {
+        console.error("Database query error:", err);
+        res.status(500).send("Error al obtener los registros de pruebas");
+    }
+});
+
+// Buscar registros de pruebas por número de competidor
+router.get(
+    "/usuarios/numeroCompetidor/:num/testing-records",
+    async (req, res) => {
+        const { num } = req.params;
+
+        try {
+            // Consulta para obtener el usuario por número de competidor
+            const [userRows] = await db.query(
+                "SELECT user_id FROM user WHERE competitor_num = ?",
+                [num]
+            );
+
+            if (userRows.length > 0) {
+                const userId = userRows[0].user_id;
+
+                // Consulta para obtener los registros de pruebas del usuario
+                const [testingRecordsRows] = await db.query(
+                    "SELECT * FROM testing_records WHERE user_id = ?",
+                    [userId]
+                );
+
+                // Devolver los registros de pruebas
+                res.json(testingRecordsRows);
+            } else {
+                res.status(404).json({
+                    error: "Usuario por número de competidor no encontrado",
+                });
+            }
+        } catch (error) {
+            console.error(
+                "Error al buscar registros de pruebas por número de competidor:",
+                error
+            );
+            res.status(500).json({ error: "Error interno del servidor" });
+        }
+    }
+);
 
 // Crear un nuevo usuario
 router.post("/staffcontrol/usuarios", async (req, res) => {
@@ -136,6 +193,84 @@ router.post("/staffcontrol/vehiculos", async (req, res) => {
         res.status(201).json(nuevoVehiculo);
     } catch (error) {
         console.error("Error al crear nuevo vehiculo:", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+// ----------------Subir test ---------------------------------------
+
+// Subir prueba de alcoholemia
+router.post("/staffcontrol/registrar-test-alcoholemia", async (req, res) => {
+    const { competitorNum, test_image } = req.body;
+
+    try {
+        // Consulta para obtener el usuario por número de competidor
+        const [userResult] = await db.query(
+            "SELECT user_id FROM user WHERE competitor_num = ?",
+            [competitorNum]
+        );
+
+        if (userResult.length === 0) {
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+
+        const userId = userResult[0].user_id;
+        const type = "alcoholemia";
+
+        // Inserción en la tabla testing_records
+        const [result] = await db.query(
+            "INSERT INTO testing_records (user_id, test_image, type) VALUES (?, ?, ?)",
+            [userId, test_image, type]
+        );
+
+        const response = {
+            id: result.insertId,
+            user_id: userId,
+            image_url: test_image,
+            type: type,
+        };
+
+        res.status(201).json(response);
+    } catch (error) {
+        console.error("Error al registrar la prueba de alcoholemia:", error);
+        res.status(500).json({ error: "Error interno del servidor" });
+    }
+});
+
+// Subir prueba de rtm
+router.post("/staffcontrol/registrar-test-mecanico", async (req, res) => {
+    const { competitorNum, test_image } = req.body;
+
+    try {
+        // Consulta para obtener el usuario por número de competidor
+        const [userResult] = await db.query(
+            "SELECT user_id FROM user WHERE competitor_num = ?",
+            [competitorNum]
+        );
+
+        if (userResult.length === 0) {
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+
+        const userId = userResult[0].user_id;
+        const type = "mecanico";
+
+        // Inserción en la tabla testing_records
+        const [result] = await db.query(
+            "INSERT INTO testing_records (user_id, test_image, type) VALUES (?, ?, ?)",
+            [userId, test_image, type]
+        );
+
+        const response = {
+            id: result.insertId,
+            user_id: userId,
+            image_url: test_image,
+            type: type,
+        };
+
+        res.status(201).json(response);
+    } catch (error) {
+        console.error("Error al registrar la prueba de alcoholemia:", error);
         res.status(500).json({ error: "Error interno del servidor" });
     }
 });
@@ -249,7 +384,7 @@ router.post("/staffcontrol/usuarioCompleto", async (req, res) => {
         for (const contact of contacts) {
             await connection.query(
                 `INSERT INTO contact (user_id, contact_reference, contact_info) VALUES (?, ?, ?)`,
-                [userId, contact.reference, contact.info]
+                [userId, contact.contact_reference, contact.contact_info]
             );
         }
 

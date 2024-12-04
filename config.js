ca2ba8419f5799649b67e65a24ca95ef4348881f
Cesar Manuel Baquiro <91351156+CesarBaquiro@users.noreply.github.com>
2024-12-04 14:35:15 -0500

    Update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PORT = process.env.DB_PORT || 3310;
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "19Berpy2019";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_NAME = process.env.DB_NAME || "safty_tq_db";
